e1c9be7f7ffcc5e4b34c88e1e7ed93d6d14e26d1
Ung Montia <iamungmonita@gmail.com>
2023-09-20 18:00:29 +0900

    Making Sidebar Responsive and Collapse

--- src/core/layouts/SideBar/index.tsx
+++ src/core/layouts/SideBar/index.tsx
@@ -1,21 +1,44 @@
+import { useEffect, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 function Sidebar() {
+  const [screenChange, setScreenChange] = useState<boolean>(false)
+  const [textChange, setTextChange] = useState<boolean>(true)
+  useEffect(() => {
+    window.addEventListener('resize', onScreenChange)
+  }, [])
+
+  function onScreenChange() {
+    if (window.innerWidth <= 640) {
+      setScreenChange(true)
+      setTextChange(false)
+    }
+    else {
+      setTextChange(true)
+      setScreenChange(false)
+    }
+  }
+  function toggleMenu() {
+    setTextChange(!textChange)
+    if (window.innerWidth <= 640) {
+      setScreenChange(!screenChange)
+    }
+  }
   return (
     <>
       <main className="flex w-full h-screen">
-        <aside className="bg-white w-64 h-screen shadow-lg p-4 space-y-3">
-          <section className="flex items-center max-sm:justify-center flex-wrap">
+        <aside className={` bg-white h-screen ${screenChange ? 'hidden' : 'flex items-center'} shadow-lg p-4 space-y-3 flex-col`}>
+          <section className="flex flex-col items-center max-sm:justify-center mb-5">
             <img src="/assets/images/logo.png" alt="" />
-            <h2 className="text-2xl">Ads Manager</h2>
+            <h2 className={`text-2xl ${textChange && 'hidden'}`}>Ads Manager</h2>
           </section>
           <section>
             <div className="text-xs">General</div>
-            <ul className=" text-sm">
+            <ul className="text-sm">
               <Link to="/ad-account">
                 <li className="py-3 pl-2 hover:bg-slate-100 rounded-md cursor-pointer transition duration-300 ease-in-out flex items-center">
                   <img src="/assets/icons/home.svg" alt="" />
-                  <span className="ml-3">Ads Manager</span>
+                  <span className={`ml-3 w-48 ${textChange && 'hidden'}`}>Ads Manager</span>
                 </li>
               </Link>
             </ul>
@@ -26,13 +49,13 @@ function Sidebar() {
               <Link to="/ad-account">
                 <li className="py-3 pl-2 hover:bg-slate-100 rounded-md cursor-pointer transition duration-300 ease-in-out flex items-center">
                   <img src="/assets/icons/users.svg" alt="" />
-                  <span className="ml-3">Ad Account Setup</span>
+                  <span className={`ml-3 w-48 ${textChange && 'hidden'}`}>Ad Account Setup</span>
                 </li>
               </Link>
               <Link to="/account-management">
                 <li className="py-3 pl-2 hover:bg-slate-100 rounded-md cursor-pointer transition duration-300 ease-in-out flex items-center">
                   <img src="/assets/icons/lock.svg" alt="" />
-                  <span className="ml-3">Account Management</span>
+                  <span className={`ml-3 w-48 ${textChange && 'hidden'}`}>Account Management</span>
                 </li>
               </Link>
             </ul>
@@ -40,8 +63,8 @@ function Sidebar() {
         </aside>
         <section className="w-full p-6 space-y-6">
           <div className="bg-white rounded-lg p-4">
-            <div className="flex items-center justify-between">
-              <img src="/assets/icons/hamburger.svg" alt="" />
+            <div className="flex items-center justify-between cursor-pointer">
+              <img src="/assets/icons/hamburger.svg" onClick={toggleMenu} alt="" />
               <div>Profile</div>
             </div>
           </div>
@@ -49,7 +72,7 @@ function Sidebar() {
             <Outlet />
           </div>
         </section>
-      </main>
+      </main >
     </>
   );
 }
