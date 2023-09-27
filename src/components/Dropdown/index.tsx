a36223099d1aa4539ccc138fd128db5ad2bc6792
Ung Montia <iamungmonita@gmail.com>
2023-09-27 22:08:56 +0900

    updated

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,49 +1,56 @@
 import { useState } from 'react';
 
-
 export type DropdownProps = {
     toggle?: boolean,
     pic?: boolean,
     title?: string,
-    customizeStyle?: string,
+    addedStyle?: string,
     items: string[],
     category: string
 }
 
-function DropdownComponent({ toggle, customizeStyle, pic, items, category }: DropdownProps) {
-
+function DropdownComponent({ toggle, addedStyle, pic, items, category }: DropdownProps) {
     const [selected, setSelected] = useState<string>('')
     const [open, setOpen] = useState<boolean>(false)
 
     return (
-        <section className=''>
+        <section>
             <div
-                className={`${!toggle ? 'block' : 'hidden'} flex items-center justify-between ${customizeStyle} border rounded-md relative`}
-                onClick={() => setOpen(!open)}
-            >
-                <div className={`flex items-center space-x-5 ${pic ? '' : 'm-2'}`}>
-                    {pic && <img src="/assets/icons/apple.svg" alt="" className='bg-black p-2.5 rounded-l-md' />}
+                className={`flex items-center justify-between border rounded-md relative ${addedStyle}  ${!toggle ? 'block' : 'hidden'}`}
+                onClick={() => setOpen(!open)}>
+                <div
+                    className={`flex items-center space-x-5 ${pic ? '' : 'm-2'}`}>
+                    {pic &&
+                        <img
+                            src="/assets/icons/apple.svg"
+                            className='bg-black p-2.5 rounded-l-md'
+                            alt="" />
+                    }
                     <h2 className='text-sm'>{selected ? selected : category}</h2>
                 </div>
-                <img src="/assets/icons/caret-down-fill.svg" alt="" className='pr-2' />
+                <img
+                    src="/assets/icons/caret-down-fill.svg"
+                    className='pr-2'
+                    alt="" />
                 {open &&
-                    <div className='border rounded-md absolute top-10 z-20
-                     bg-white w-full'>
+                    <div
+                        className='border rounded-md absolute top-10 z-20 bg-white w-full'>
                         {items.map(((item, index) =>
-                            <div key={index} className={`hover:bg-slate-200 p-2`} onClick={() => {
-                                if (item.toLowerCase() !== selected.toLowerCase()) {
-                                    setSelected(item)
-                                    setOpen(false)
-                                }
-                            }}>
+                            <div
+                                key={index}
+                                className={`hover:bg-slate-100 p-2`}
+                                onClick={() => {
+                                    if (item.toLowerCase() !== selected.toLowerCase()) {
+                                        setSelected(item)
+                                        setOpen(false)
+                                    }
+                                }}>
                                 {item}
                             </div>
                         ))}
                     </div>
                 }
             </div>
-
-
         </section >
     );
 }
