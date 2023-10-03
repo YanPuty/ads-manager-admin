b98e929e2e4923369312713179c267b4cb86f2d5
YanPuty <yanputy123@gmail.com>
2023-10-04 05:41:26 +0700

    Defined Account Status

--- src/service/ad-accounts.ts
+++ src/service/ad-accounts.ts
@@ -2,5 +2,5 @@ import { GET } from '../core';
 import { AdAccounts } from '../models/AdAccounts';
 
 export const getAllAdsAccounts = (): Promise<AdAccounts> => {
-  return GET<AdAccounts>('me/adaccounts?fields=name,adaccounts,account_id');
+  return GET<AdAccounts>('me/adaccounts?fields=name,adaccounts,account_id,account_status');
 }
