afb16f619fb9c728380dc75b3134b2a371a4b713
YanPuty <yanputy123@gmail.com>
2023-10-09 10:05:37 +0700

    Fixed Error in Console Log

--- src/pages/AdsManager/index.tsx
+++ src/pages/AdsManager/index.tsx
@@ -1,21 +1,27 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { filter, find, flatMap } from 'lodash';
-import React, { useEffect, useState } from 'react';
+import { filter, find, flatMap } from "lodash";
+import React, { useEffect, useState } from "react";
 
-import { Alert, DateRangePicker, DateTypeParam, Dropdown, ItemList } from '../../core';
-import { AccountStatus } from '../../enums/AccountStatus';
-import { useApi } from '../../hooks';
-import { AdAccountsData } from '../../models/AdAccounts';
-import { AdsSetsData } from '../../models/AdsSets';
-import { InSights } from '../../models/Insights';
-import { getAllAdsAccounts } from '../../service/ad-accounts';
-import { getAdsSets } from '../../service/ads-manager';
-import { getInsightsByAdId } from '../../service/insights';
+import {
+  Alert,
+  DateRangePicker,
+  DateTypeParam,
+  Dropdown,
+  ItemList,
+} from "../../core";
+import { AccountStatus } from "../../enums/AccountStatus";
+import { useApi } from "../../hooks";
+import { AdAccountsData } from "../../models/AdAccounts";
+import { AdsSetsData } from "../../models/AdsSets";
+import { InSights } from "../../models/Insights";
+import { getAllAdsAccounts } from "../../service/ad-accounts";
+import { getAdsSets } from "../../service/ads-manager";
+import { getInsightsByAdId } from "../../service/insights";
 
 function AdsManagerListingPage() {
   const [selectedId, setSelectedId] = useState<string | null>(null);
   const [accountStatus, setAccountStatus] = useState<number | null>(null);
-  const [searchInput, setSearchInput] = useState<string>('');
+  const [searchInput, setSearchInput] = useState<string>("");
 
   const [adsSetsDate, setAdsSetData] = useState<AdsSetsData[]>([]);
   const [adAccounts, setAdAccounts] = useState<AdAccountsData[]>([]);
@@ -37,7 +43,7 @@ function AdsManagerListingPage() {
     if (allAdsAccount?.data.length) {
       const mappedData = allAdsAccount.data.map((item) => ({
         ...item,
-        name: item.name.concat(' (', item.account_id, ')'),
+        name: item.name.concat(" (", item.account_id, ")"),
       }));
       setAdAccounts(mappedData);
       setSelectedId(mappedData[0].id);
@@ -60,7 +66,7 @@ function AdsManagerListingPage() {
     if (adsSetsDate.length && since && until) {
       const queryParam = { since, until };
       const allInSightsPromise = adsSetsDate.map((item) =>
-        getInsightsByAdId(item.id, queryParam)
+        getInsightsByAdId(item.id, queryParam),
       );
       setIsFetchingInsight(true);
       Promise.all(allInSightsPromise).then((insights: InSights[]) => {
@@ -89,44 +95,44 @@ function AdsManagerListingPage() {
       (item) =>
         item.name
           .toLowerCase()
-          .replace(/\s/g, '')
-          .includes(searchInput.replace(/\s/g, '').toLowerCase()) ||
+          .replace(/\s/g, "")
+          .includes(searchInput.replace(/\s/g, "").toLowerCase()) ||
         item.campaign.name
           .toLowerCase()
-          .replace(/\s/g, '')
-          .includes(searchInput.replace(/\s/g, '').toLowerCase()) ||
+          .replace(/\s/g, "")
+          .includes(searchInput.replace(/\s/g, "").toLowerCase()) ||
         item.status
           .toLowerCase()
-          .replace(/\s/g, '')
-          .includes(searchInput.replace(/\s/g, '').toLowerCase()) ||
+          .replace(/\s/g, "")
+          .includes(searchInput.replace(/\s/g, "").toLowerCase()) ||
         item.insight?.objective
           .toLowerCase()
-          .replace(/\s/g, '')
-          .includes(searchInput.replace(/\s/g, '').toLowerCase()) ||
+          .replace(/\s/g, "")
+          .includes(searchInput.replace(/\s/g, "").toLowerCase()) ||
         item.insight?.reach
           .toLowerCase()
-          .replace(/\s/g, '')
-          .includes(searchInput.replace(/\s/g, '').toLowerCase()) ||
+          .replace(/\s/g, "")
+          .includes(searchInput.replace(/\s/g, "").toLowerCase()) ||
         item.insight?.impressions
           .toLowerCase()
-          .replace(/\s/g, '')
-          .includes(searchInput.replace(/\s/g, '').toLowerCase()) ||
+          .replace(/\s/g, "")
+          .includes(searchInput.replace(/\s/g, "").toLowerCase()) ||
         item.insight?.spend
           .toLowerCase()
-          .replace(/\s/g, '')
-          .includes(searchInput.replace(/\s/g, '').toLowerCase())
+          .replace(/\s/g, "")
+          .includes(searchInput.replace(/\s/g, "").toLowerCase()),
     );
     setItems(filterSearch);
   }, [searchInput, adsSetsDate]);
 
-  const formatNumber = (amount = '0') => {
+  const formatNumber = (amount = "0") => {
     if (Number.isNaN(amount)) {
       return 0;
     }
     return Number(Number(amount) / 100).toFixed(2);
   };
 
-  const formatNumberWithComma = (amount = '0') => {
+  const formatNumberWithComma = (amount = "0") => {
     if (Number.isNaN(amount)) {
       return 0;
     }
@@ -158,7 +164,7 @@ function AdsManagerListingPage() {
           className="input-search"
           type="text"
           placeholder="Search Filter"
-          style={{ backgroundImage: 'url(/assets/icons/search.svg)' }}
+          style={{ backgroundImage: "url(/assets/icons/search.svg)" }}
           onChange={(e) => setSearchInput(e.target.value)}
         />
         {adAccounts.length > 0 && (
@@ -184,7 +190,7 @@ function AdsManagerListingPage() {
         )}
       </div>
 
-      <div className="overflow-scroll" style={{ maxHeight: '80vh' }}>
+      <div className="overflow-scroll" style={{ maxHeight: "80vh" }}>
         <table className="table mt-5 relative">
           <thead>
             <tr>
@@ -199,80 +205,13 @@ function AdsManagerListingPage() {
               <th className="sticky top-0 px-6 py-3 ">Amount Spend</th>
             </tr>
           </thead>
-          <tr>
-            <td colSpan={9}>
-              {loading && (
-                <div
-                  role="status"
-                  className="p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
-                >
-                  <div className="flex items-center justify-between">
-                    <div>
-                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-                      <div className="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-                    </div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                  </div>
-                  <div className="flex items-center justify-between pt-4">
-                    <div>
-                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-                      <div className="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-                    </div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                  </div>
-                  <div className="flex items-center justify-between pt-4">
-                    <div>
-                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-                      <div className="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-                    </div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                  </div>
-                  <div className="flex items-center justify-between pt-4">
-                    <div>
-                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-                      <div className="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-                    </div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                  </div>
-                  <div className="flex items-center justify-between pt-4">
-                    <div>
-                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-                      <div className="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-                    </div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                    <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-                  </div>
-                </div>
-              )}
-            </td>
-          </tr>
-          {!loading && items.length === 0 && (
-            <div className="py-10">
-              <p className="absolute left-[50%] translate-x-[-50%] font-light italic">
-                There is no available data for this user
-              </p>
-            </div>
-          )}
-          {items.length > 0 && !loading && (
-            <tbody>
-              {items.map((item, index) => (
+          <tbody>
+            {items.length > 0 &&
+              !loading &&
+              items.map((item, index) => (
                 <tr key={item.id}>
                   <td className="text-center">{index + 1}</td>
-                  <td className="truncate" style={{ maxWidth: '200px' }}>
+                  <td className="truncate" style={{ maxWidth: "200px" }}>
                     {item.name}
                   </td>
                   <td className="text-center">
@@ -287,23 +226,94 @@ function AdsManagerListingPage() {
                   </td>
                   <td className="text-center">
                     {fetchingInSightWrapper(
-                      formatNumberWithComma(item.insight?.reach)
+                      formatNumberWithComma(item.insight?.reach),
                     )}
                   </td>
                   <td className="text-center">
                     {fetchingInSightWrapper(
-                      formatNumberWithComma(item.insight?.impressions)
+                      formatNumberWithComma(item.insight?.impressions),
                     )}
                   </td>
                   <td className="text-center">
                     {fetchingInSightWrapper(
-                      item.insight?.spend ? `$ ${item.insight?.spend}` : ''
+                      item.insight?.spend ? `$ ${item.insight?.spend}` : "",
                     )}
                   </td>
                 </tr>
               ))}
-            </tbody>
-          )}
+            {loading && (
+              <tr>
+                <td colSpan={9}>
+                  <div
+                    role="status"
+                    className="p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+                  >
+                    <div className="flex items-center justify-between">
+                      <div>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                      </div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                    </div>
+                    <div className="flex items-center justify-between pt-4">
+                      <div>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                      </div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                    </div>
+                    <div className="flex items-center justify-between pt-4">
+                      <div>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                      </div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                    </div>
+                    <div className="flex items-center justify-between pt-4">
+                      <div>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                      </div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                    </div>
+                    <div className="flex items-center justify-between pt-4">
+                      <div>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                      </div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            )}
+            {!loading && items.length === 0 && (
+              <tr>
+                <td>
+                  <div className="py-10">
+                    <p className="absolute left-[50%] translate-x-[-50%] font-light italic">
+                      There is no available data for this user
+                    </p>
+                  </div>
+                </td>
+              </tr>
+            )}
+          </tbody>
         </table>
       </div>
     </div>
